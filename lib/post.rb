b3acc910dd44893b1ab8230130c4750779182075
shreya-sridhar <shreyasridhar1109@gmail.com>
2020-11-19 23:30:16 +0000

    songs & posts

---
+++ lib/post.rb
@@ -0,0 +1,15 @@
+class Post
+    attr_accessor :title
+    attr_reader :author
+
+    def initialize
+        @title = title
+    end
+
+    def author=(author)
+        @author=author
+    end
+
+
+end   
+
---
+++ lib/song.rb
@@ -0,0 +1,13 @@
+class Song
+    attr_accessor :title
+    attr_reader :artist
+
+    def initialize
+        @title = title
+    end
+
+    def artist=(artist)
+        @artist = artist
+    end
+
+end
